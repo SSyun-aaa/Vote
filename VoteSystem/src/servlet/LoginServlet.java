09e8502c8c1b0e97581837badfaf59305a838e9d
eiki <eiki@172.30.12.194>
2018-06-11 15:08:52 +0900

    ログインサーブレット作成

--- VoteSystem/src/servlet/LoginServlet.java
+++ VoteSystem/src/servlet/LoginServlet.java
@@ -2,12 +2,17 @@ package servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.PasswordDAO;
+import dao.UserDAO;
+import model.PasswordBean;
+import model.UserBean;
 
 /**
  * Servlet implementation class LoginServlet
@@ -29,7 +34,6 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 
 	}
 
@@ -38,7 +42,37 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+
+		String id = request.getParameter("userid");
+		String password = request.getParameter("password");
+
+		PasswordDAO passwordDAO = new PasswordDAO();
+		PasswordBean userpass = new PasswordBean();
+
+		userpass = passwordDAO.searchUser(id, password);
+
+		String path = "";
+		if(userpass != null){
+			UserBean userBean = new UserBean();
+			UserDAO userDAO = new UserDAO();
+
+			userBean = userDAO.getUser(id);
+			session.setAttribute("loginUser",userBean);
+
+			if(userBean.getAuthority().equals("U")){
+				path = "contents.jsp";
+			}else{
+				path = "*";
+			}
+		}else{
+			path = "login.jsp";
+		}
+		request.getRequestDispatcher(path).forward(request, response);
+
+
 	}
 
 }
