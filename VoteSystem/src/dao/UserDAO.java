13e08869173c9c65ddccfda7a8481839f0a3e754
eiki <eiki@172.30.24.191>
2018-06-25 14:21:23 +0900

    新規登録のサーブレットの実装

--- VoteSystem/src/dao/UserDAO.java
+++ VoteSystem/src/dao/UserDAO.java
@@ -3,28 +3,28 @@ package dao;
 import model.UserBean;
 
 public class UserDAO extends DaoBase {
-	
+
 	public UserBean getUser(String id){
 		UserBean user = new UserBean();
 		try{
 		super.connection();
-		
+
 		String sql ="select * from user where userID = ?";
 		stmt = con.prepareStatement(sql);
-		
+
 		stmt.setString(1, id);
 		rs=stmt.executeQuery();
 		rs.next();
-		
+
 		user.setUserID(rs.getString(1));
 		user.setUserName(rs.getString(2));
 		user.setCreateDate(rs.getDate(3));
 		user.setAuthority(rs.getString(4));
 		user.setSex(rs.getString(5));
 		user.setBirthday(rs.getDate(6));
-		
+
 		}catch(Exception e){
-			
+
 		}finally {
 			try{
 				super.DbClose();
@@ -35,24 +35,23 @@ public class UserDAO extends DaoBase {
 		return user;
 		//ユーザが見つかれば該当ユーザ情報を、いなければnullをかえす
 	}
-	
+
 	public void userInsert(UserBean user){
 		//渡されたユーザ情報をユーザテーブルへ挿入
 		try{
 			//super.DbOpen();
 			super.connection();
-			String sql  ="insert into user(userID,userName,authority,sex,birthday) values(?,?,?,?,?)";
-			
+			String sql  ="insert into user(userID,userName,sex,birthday) values(?,?,?,?,?)";
+
 			stmt = con.prepareStatement(sql);
-			
+
 			stmt.setString(1, user.getUserID());
 			stmt.setString(2, user.getUserName());
-			stmt.setString(3, user.getAuthority());
-			stmt.setString(4, user.getSex());
-			stmt.setDate(5, user.getBirthday());
-			
+			stmt.setString(3, user.getSex());
+			stmt.setDate(4, user.getBirthday());
+
 			rsno = stmt.executeUpdate();
-				
+
 		}catch(Exception e){
 			user=null;
 		}finally {
@@ -63,20 +62,20 @@ public class UserDAO extends DaoBase {
 			}
 		}
 	}
-	
+
 	public void userDelete(String id){
 		try{
 			//super.DbOpen();
 			super.connection();
-			
+
 			String sql ="Delete from user where userID = ?";
 			stmt = con.prepareStatement(sql);
-			
+
 			stmt.setString(1,id);
 			rsno=stmt.executeUpdate();
-			
+
 			}catch(Exception e){
-				
+
 			}finally {
 				try{
 					super.DbClose();
@@ -85,5 +84,5 @@ public class UserDAO extends DaoBase {
 				}
 			}
 	}
-	
+
 }
